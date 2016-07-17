a0fc19e7f84b5238646351019c404ae09f1304b7
Eli Goldweber <eli.goldweber@ge.com>
2016-07-17 15:02:43 -0700

    delete

--- microserviceChild.js
+++
@@ -1,7 +0,0 @@
-process.on('message', function(m) {
-  // Do work  (in this case just up-case the string
-  m = m.toUpperCase();
-
-  // Pass results back to parent process
-  process.send(m.toUpperCase(m));
-});
